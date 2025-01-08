76a2e5074f9056ed24a2b7daa4d6eb2b49762930
vladi1ck <90263460+vladi1ck@users.noreply.github.com>
2025-01-08 14:42:45 +0300

    Update celery.py

--- admin_panel/admin_panel/celery.py
+++ admin_panel/admin_panel/celery.py
@@ -2,7 +2,7 @@ import os
 
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'yourproject.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'admin_panel.settings')
 app = Celery('bot_market')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
